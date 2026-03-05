939059b218cc8dcec7043a0d78417e978bdc705a
Yuuri Inoue <yuuri041125@gmail.com>
2026-03-06 00:25:26 +0900

    feat:簡単な画面構成を実装

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,14 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function Home() {
   const days = Array.from({ length: 35 }, (_, i) => ({
     day: i + 1,
     hasStudy: Math.random() > 0.7,
   }));
+  const [selectedDay, setSelectedDay] = useState<number | null>(null);
+  const [showInput, setShowInput] = useState(false);
+  const [title, setTitle] = useState('');
 
   return (
     <View style={styles.container}>
@@ -13,13 +17,42 @@ export default function Home() {
       <View style={styles.calendar}>
         <View style={styles.grid}>
           {days.map((item) => (
-            <View key={item.day} style={styles.day}>
+            <Pressable
+              key={item.day}
+              style={styles.day}
+              onPress={() => setSelectedDay(item.day)}
+            >
               <Text style={styles.date}>{item.day}</Text>
               <Text>{item.hasStudy ? '🍅' : '🌱'}</Text>
-            </View>
+            </Pressable>
           ))}
         </View>
       </View>
+      {selectedDay && (
+        <View style={styles.log}>
+          <Text style={styles.logTitle}>{selectedDay}日のログ</Text>
+          <Text>React Native勉強</Text>
+          <Text>Next.js記事</Text>
+        </View>
+      )}
+      <Pressable style={styles.addButton} onPress={() => setShowInput(true)}>
+        <Text style={styles.addText}>＋</Text>
+      </Pressable>
+
+      {showInput && (
+        <View style={styles.inputBox}>
+          <TextInput
+            placeholder="何を勉強しましたか？"
+            value={title}
+            onChangeText={setTitle}
+            style={styles.input}
+          />
+
+          <Pressable style={styles.saveButton}>
+            <Text style={{ color: 'white' }}>保存</Text>
+          </Pressable>
+        </View>
+      )}
     </View>
   );
 }
@@ -59,5 +92,56 @@ const styles = StyleSheet.create({
   },
   date: {
     fontSize: 10,
+  },
+  log: {
+    marginTop: 20,
+    width: '90%',
+    padding: 10,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+  },
+
+  logTitle: {
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 40,
+    right: 30,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: '#ff6347',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  addText: {
+    fontSize: 30,
+    color: 'white',
+  },
+  inputBox: {
+    position: 'absolute',
+    bottom: 120,
+    width: '90%',
+    backgroundColor: 'white',
+    padding: 15,
+    borderRadius: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    borderRadius: 5,
+    marginBottom: 10,
+  },
+
+  saveButton: {
+    backgroundColor: '#ff6347',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
   },
 });
